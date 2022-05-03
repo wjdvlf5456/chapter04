46254b2e694e3badea4621fb648e92730fad395f
최정필 <wjdvlf1407@gmail.com>
2022-05-03 16:44:49 +0900

    Ex09 HashMap

--- src/com/javaex/ex09/Point.java
+++ src/com/javaex/ex09/Point.java
@@ -7,29 +7,36 @@ public class Point {
 	private int y;
 
 	// 생성자
+	public Point() {
+		super(); //Object()  
+	}
+
+
 	public Point(int x, int y) {
+		super();  //Object()  
 		this.x = x;
 		this.y = y;
 	}
 
-	// 메소드 - gs
+
+	// 메소드 gs
 	public int getX() {
 		return x;
-	}
-
-	public int getY() {
-		return y;
 	}
 
 	public void setX(int x) {
 		this.x = x;
 	}
 
+	public int getY() {
+		return y;
+	}
+
 	public void setY(int y) {
 		this.y = y;
 	}
 
-	// 메소드 - 일반
+	// 메소드 일반
 	@Override
 	public String toString() {
 		return "Point [x=" + x + ", y=" + y + "]";
