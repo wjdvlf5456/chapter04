82311fa08729532624108e1c307c405d24db16f7
최정필 <wjdvlf1407@gmail.com>
2022-05-03 16:37:27 +0900

    Ex02

--- src/com/javaex/ex02/MyListApp.java
+++ src/com/javaex/ex02/MyListApp.java
@@ -4,6 +4,8 @@ public class MyListApp {
 
 	public static void main(String[] args) {
 
+		////////////////////////////////////
+		// Point 관리
 		MyList pList = new MyList();
 
 		Point p01 = new Point(3, 5);
@@ -15,26 +17,29 @@ public class MyListApp {
 		System.out.println(pList.size());
 		System.out.println(pList.get(0).toString());
 
-		System.out.println("==============================================");
+		System.out.println("-------------");
 		for (int i = 0; i < pList.size(); i++) {
-			System.out.println(pList.get(0).toString());
+			// System.out.println(pList.get(i).toString());
 			Point p = (Point) pList.get(i);
-			p.getX();
+			System.out.println(p.getX());
 		}
 
+		System.out.println("================================");
+
+		////////////////////////////////////
+		// 원 관리
 		MyList cList = new MyList();
 
 		Circle c01 = new Circle(3);
-		Circle c02 = new Circle(9);
+		Circle c02 = new Circle(13);
 
 		cList.add(c01);
 		cList.add(c02);
 		cList.add(p01);
 
 		for (int i = 0; i < cList.size(); i++) {
-			System.out.println(cList.get(0).toString());
-			Circle c = (Circle) cList.get(i);
-			c.getRadius();
+			System.out.println(cList.get(i).toString());
+			// System.out.println( ( (Circle)cList.get(i) ).getRadius() );
 
 		}
 
